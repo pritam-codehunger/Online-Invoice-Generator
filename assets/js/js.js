40b9ba9c717dbf31ba467bb9a2a4f79debed7f61
Pritam Makwana <pritam@codehunger.in>
2024-10-23 18:06:12 +0530

    -Google Translate in menu bar improve ui remove -Contact hover show google translate popup this -Invoice generator in color change with change header color this -Bank Details in light color is change with color -show console in js errors all solve (related include file header,footer,modals)

--- assets/js/js.js
+++ assets/js/js.js
@@ -3,21 +3,26 @@ function loadHTML(containerId, file) {
     fetch(file)
         .then(response => response.text())
         .then(data => {
-            document.getElementById(containerId).innerHTML = data;
+            const container = document.getElementById(containerId);
+            if (container) {
+                container.innerHTML = data;
+            } else {
+                console.error(`Element with id '${containerId}' not found.`);
+            }
         })
         .catch(error => console.error('Error loading file:', error));
 }
 loadHTML('header', 'header.html');
 loadHTML('footer', 'footer.html');
-loadHTML('modals', 'modals.html');
 // header footer add
+
 // color changes
 $(document).ready(function () {
     $('#primaryColorPicker').on('input', function () {
         const newColor = $(this).val();
-        document.documentElement.style.setProperty('--primary-color', newColor);
+        document.documentElement.style.setProperty('--primary-change-color', newColor);
         const lightColor = `${newColor}21`;
-        document.documentElement.style.setProperty('--primary-light-color', lightColor);
+        document.documentElement.style.setProperty('--primary-change-light-color', lightColor);
         $('.card-border').css('border-color', newColor);
     });
 });
@@ -28,63 +33,91 @@ $(document).ready(function () {
         $('.color-swatch').removeClass('active');
         $(this).addClass('active');
         const selectedColor = $(this).data('color');
-        document.documentElement.style.setProperty('--primary-color', selectedColor);
+        document.documentElement.style.setProperty('--primary-change-color', selectedColor);
         const lightColor = `${selectedColor}21`;
-        document.documentElement.style.setProperty('--primary-light-color', lightColor);
+        document.documentElement.style.setProperty('--primary-change-light-color', lightColor);
         $('.card-border').css('border-color', selectedColor);
     });
 });
 // end color changes
-// google Translate 
+
+// google Translate in menu bar
+
+// add google Translate 
 function googleTranslateElementInit() {
     new google.translate.TranslateElement({
         pageLanguage: 'en',
-        includedLanguages: 'en,es,fr,de,it', // Add the languages you want to support
-        autoDisplay: false
+        includedLanguages: 'en,es,fr,de,it,hi', 
+        autoDisplay: false,
     }, 'google_translate_element');
 }
 //end google Translate 
-setInterval(function () {
-    if ($('html').hasClass('translated-ltr') ||
-        (document.getElementsByClassName("skiptranslate")[1].style.visibility == 'visible' &&
-            document.getElementsByClassName("skiptranslate")[0].style.display != 'none')) {
-        $('.navbar').css('margin-top', '30px');
-    } else {
-        $('.navbar').css('margin-top', '0px');
-    }
-}, 500);
 
-// Create a <style> element
+// css add for google Translate 
 const style = document.createElement('style');
-
-// Define your CSS rules
 style.innerHTML = `
-.skiptranslate {
-};
+
+/* Google Translate powered by remove */
 .goog-logo-link {
     display:none !important;
   }
 .goog-te-gadget {
-      color: transparent !important;
-    }
+    color: transparent !important;
+}
+/* end Google Translate powered by remove */
+
+/* Google Translate fully hight set related your menu */
+#google_translate_element { 
+    height: 43px !important;
+    overflow: hidden !important; 
+    } 
+/*end Google Translate fully hight set related your menu */
+
+
+/* Google Translate select option in give desing */
 .goog-te-gadget .goog-te-combo {
-    margin: 9px 0;
-       height: 35px;
-       width: 90px;
+  display: block;
+    width: 100%;
+    padding: 0.375rem 0.75rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    color: #495057;
+    background-color: #fff;
+    background-clip: padding-box;
+    border: 1px solid #ced4da;
+    border-radius: 0.25rem;
+    transition: border-color .15s ease-in-out, box-shadow .15s ease-in-out; 
    } 
+/*end Google Translate select option in give desing */
 
 
+/* Hide the floating Google Translate widget */
+.goog-logo-link {
+    display: none !important;
+}
+/*end Hide the floating Google Translate widget */
 
-#google_translate_element { height: 26px !important; overflow: hidden !important; } 
+/* Hide Google Translate top in goole menu */
+.VIpgJd-ZVi9od-ORHb-OEVmcd{
+  display: none !important;
+}
+/* end Hide Google Translate top in goole menu */
 
-`
+/* Hide Google Translate top in goole menu in space problem solve */
+.goog-te-banner-frame.skiptranslate {
+    display: none !important;
+} 
+body {
+    top: 0px !important; 
+}
+/* Hide Google Translate top in goole menu in space problem solve */
 
+/* Hide Google Translate contant right click or hover show pop up this remove */
+#goog-gt-tt{display: none !important; top: 0px !important; }
+/*end  Hide Google Translate contant right click or hover show pop up this remove */
+`;
 
-
-// Append the <style> element to the document's head
 document.head.appendChild(style);
+//end css add for google Translate
 
-// $(window).load(function(){
-//     $(".goog-logo-link").empty();
-//     $('.goog-te-gadget').html($('.goog-te-gadget').children());
-// })
+//end google Translate in menu bar
